8a64fbca3e986ba550224f8511ec5343ff77c50f
MaritimeEng <t231003@edu.kaiyodai.ac.jp>
2026-09-23 12:44:44 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -4,5 +4,6 @@
 //  （index.html / help.html / admin.html の3つがこの値を使います）
 // =====================================================
 const APP_CONFIG = {
-  gasUrl: "https://script.google.com/macros/s/AKfycbzOEHtYNVJuuOpYz3muhUO9_m6pXfwOWMGM9UhOW6Vs9L7d4X9LfLpql9rpObB9dxdM/exec"   // 例: "https://script.google.com/macros/s/XXXXXXXX/exec"
+  gasUrl: 
+"https://script.google.com/macros/s/AKfycbzOEHtYNVJuuOpYz3muhUO9_m6pXfwOWMGM9UhOW6Vs9L7d4X9LfLpql9rpObB9dxdM/exec"
 };
